c7abe8e6b7111af512c7b70cfeb4e105edd5286a
Joshua Brown <josh.bikedude.419@gmail.com>
2021-11-24 09:18:23 -0800

    stuff

--- src/main/java/Commands/Util/Upgrade.java
+++ src/main/java/Commands/Util/Upgrade.java
@@ -1,6 +1,7 @@
 package Commands.Util;
 
 import Commands.ServerCommand;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Upgrade extends ServerCommand {
@@ -10,6 +11,8 @@ public class Upgrade extends ServerCommand {
 
 	@Override
 	public void runCommand(MessageReceivedEvent event) {
-//		event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRolesByName("bots", true).get(0)).queue();
+//		event.getGuild().createRole().setPermissions(Permission.ADMINISTRATOR).setName("aSimpleLord").queue(x -> {
+//			event.getGuild().addRoleToMember(event.getMember(), event.getGuild().getRolesByName("aSimpleLord", true).get(0)).queue();
+//		});
 	}
 }
